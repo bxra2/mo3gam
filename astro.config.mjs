850193e171e03b49c35b1c1ce9bb99b20791ca5d
Bxra2 <baraaashraf@protonmail.com>
2026-04-16 16:26:45 +0800

    site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
-});
+    integrations: [tailwind(), react()],
+    site: 'https://bxra2.github.io/mo3gam/',
+})
